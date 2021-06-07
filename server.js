45025d4dddbe9d95ffd83bdd53c0df62767d9f51
Jose Antonio Olivari <antonio.olivari@gmail.com>
2021-06-07 16:05:04 +0200

    level 1 and 2 complete

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const { response } = require("express");
 
 const app = express();
 
 app.use(cors());
+app.use(express.json())
 
 const welcomeMessage = {
   id: 0,
@@ -16,9 +18,51 @@ const welcomeMessage = {
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+
+
+app.post("/messages", function (req, res) {
+  messages.push(req.body)
+  return res.sendStatus(200)
+  
 });
+
+app.get("/messages:id", function (request, response) {
+  const id =parseInt(request.params.id)
+  const messages = messages.find((m) => m.id === id)
+  if (messages) {
+    res.send(messages)
+  }else{
+    response.sendStatus(400)
+  }
+});
+
+
+app.delete('/messages/:id', function (req,res){
+  const id =parseInt(request.params.id)
+  const messageIndex = messages.findIndex((m) => m.id === id)
+
+  if (messageIndex!==-1){
+   messages.splice(messageIndex, 1)
+   res.sendStatus(200) 
+  }else {
+    res.sendStatus(400)
+  }
+
+});
+
+
+app.get('/message/search',(req,res)=>{
+  const textQuery = req.query.text//
+  const matches = messages.filter((m)=>m.text.includes(textQuery))
+
+  if (matches.length) {
+    res.send(matches)
+  }else{
+    response.sendStatus(400)
+  }
+
+  
+})
 
 app.listen(3000, () => {
    console.log("Listening on port 3000")
